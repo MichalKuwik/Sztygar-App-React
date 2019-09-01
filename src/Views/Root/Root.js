7a8e5015fa50afa782cdf935b9f41b5f66df11f9
Michał Kuwik <Michal7074@poczta.fm>
2019-09-01 17:58:04 +0200

    reorganize first basic project's  structure

---
+++ src/Views/Root/Root.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import styles from './Root.module.css';
+
+
+class Root extends Component {
+  state = {  }
+
+  render() { 
+    return (  
+      <div>
+        App
+      </div>
+    );
+  }
+}
+ 
+export default Root;
