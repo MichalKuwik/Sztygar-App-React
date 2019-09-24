04d1a4eb8a727abc64858ff24e4601192a44ce30
Michał Kuwik <Michal7074@poczta.fm>
2019-09-24 19:26:11 +0200

    Updates

---
+++ precache-manifest.da1a49c8c2b39c8462cec941a33bb79c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d198b76eda5e41ad5e35c38598631c8c",
+    "url": "/Sztygar-App-React/index.html"
+  },
+  {
+    "revision": "c6176c64da44b7ee8ea1",
+    "url": "/Sztygar-App-React/static/css/main.19af848a.chunk.css"
+  },
+  {
+    "revision": "9a7343b8790d71890378",
+    "url": "/Sztygar-App-React/static/js/2.b4db437d.chunk.js"
+  },
+  {
+    "revision": "c6176c64da44b7ee8ea1",
+    "url": "/Sztygar-App-React/static/js/main.21275b56.chunk.js"
+  },
+  {
+    "revision": "1a7e753a089d7e303ca5",
+    "url": "/Sztygar-App-React/static/js/runtime~main.38d9c9f8.js"
+  },
+  {
+    "revision": "96a456f775b3760698f99291b6799bb3",
+    "url": "/Sztygar-App-React/static/media/miner.96a456f7.png"
+  },
+  {
+    "revision": "c846c87ca75baee108ca850660dfca92",
+    "url": "/Sztygar-App-React/static/media/tools.c846c87c.png"
+  }
+]);
